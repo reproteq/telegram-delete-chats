02e03ffdc2396247605c9d4d2e92e3ff41a9c9d3
Alejandro Gregori Tomás <reproteq@gmail.com>
2024-08-29 00:05:26 +0200

    easy and fast
    clean chats

---
+++ telegram-delete-chats.py
@@ -0,0 +1,24 @@
+from telethon.sync import TelegramClient
+
+# Configuración de la API de Telegram
+# https://my.telegram.org/apps
+api_id = 'TU_API_ID'
+api_hash = 'TU_API_HASH'
+phone_number = 'TU_NUMERO_DE_TELEFONO'  # Ejemplo: '+34123456789'
+
+# Inicializa el cliente de Telethon
+client = TelegramClient('session_name', api_id, api_hash)
+
+async def borrar_todos_los_chats():
+    async with client:
+        async for dialog in client.iter_dialogs():
+            try:
+                await client.delete_dialog(dialog.id)
+                print(f"Chat con {dialog.name} borrado.")
+            except Exception as e:
+                print(f"No se pudo borrar el chat con {dialog.name}: {e}")
+
+# Ejecuta la función para borrar todos los chats
+with client:
+    client.loop.run_until_complete(borrar_todos_los_chats())
+
